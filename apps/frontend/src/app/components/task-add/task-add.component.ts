8e5ef9ffd625a5365630603b6fdd59a07836965d
michalwolanin <michalwolanin02@gmail.com>
2023-10-15 00:53:28 +0200

    Added the frontend application view.

--- apps/frontend/src/app/components/task-add/task-add.component.ts
+++ apps/frontend/src/app/components/task-add/task-add.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TasksListComponent } from '../tasks-list/tasks-list.component';
 
 @Component({
   selector: 'to-do-task-add',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TasksListComponent],
   templateUrl: './task-add.component.html',
   styleUrls: ['./task-add.component.scss'],
 })
 export class TaskAddComponent {
   value?: string;
+
+  addTask() {}
 }
--- apps/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ apps/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TasksListComponent} from "../../components/tasks-list/tasks-list.component";
-import {TaskAddComponent} from "../../components/task-add/task-add.component";
+import { TasksListComponent } from '../../components/tasks-list/tasks-list.component';
+import { TaskAddComponent } from '../../components/task-add/task-add.component';
 
 @Component({
   selector: 'to-do-dashboard',
